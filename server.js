138981043f4963d12c82d924b743b673c79f4b36
helloprof <suluxan.seneca@gmail.com>
2024-02-08 11:44:04 -0500

    serving html from /news route

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require('express'); // "require" the Express module
 const app = express(); // obtain the "app" object
 const HTTP_PORT = 8080; // assign a port
 const newsService = require("./modules/newsService")
+const path = require("path")
+
+app.use(express.static("public"))
 
 app.get("/", (req, res) => {
     // newsService.getNews().then((data) => {
@@ -13,8 +16,16 @@ app.get("/", (req, res) => {
 
 app.get("/news", (req, res) => {
     newsService.getNews().then((data) => {
-        res.send(data)
+        res.sendFile(path.join(__dirname, "/views/news.html"))
     })
+})
+
+app.get("/about", (req, res) => {
+    // newsService.getNews().then((data) => {
+    //     res.send(data)
+    // })
+    
+    res.send("about")
 })
 
 newsService.initialize().then(() => {
