aa1a9638efc2a51f7b30291557b8f6c38505b461
helloprof <suluxan.seneca@gmail.com>
2024-04-11 10:03:39 -0400

    adding register and login functions with bcrypt and mongoose

--- modules/userService.js
+++ modules/userService.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose")
 const env = require("dotenv")
-
 env.config()
+const bcrypt = require('bcryptjs');
 
 let Schema = mongoose.Schema
 
@@ -37,8 +37,62 @@ function initialize() {
     })
 }
 
+function register(userData) {
+    return new Promise((resolve, reject) => {
+        if (userData.password != userData.password2) {
+            reject("Passwords do not match")
+        }
+
+        bcrypt.hash(userData.password, 10) 
+        .then((hash) => {
+            userData.password = hash
+
+            const newUser = new User(userData)
+            newUser.save().then(() => {
+                resolve("User registered successfully")
+            }).catch((err) => {
+                if (err.code == 11000) {
+                    reject("Username already taken")
+                } else {
+                    reject("An error occurred: "+err) 
+                }
+            })
+        }).catch((err) => {
+            reject("Encryption Error: "+err)
+        })
+    })
+}
+
+function login(userData) {
+    return new Promise((resolve, reject) => {
+        User.findOne({username: userData.username})
+        .exec()
+        .then((user) => {
+            bcrypt.compare(userData.password, user.password)
+            .then((success) => {
+                if (success) {
+                    // add loginHistory stuff here
+                    // let newUserHistory = {dateTime: new Date(), userAgent: userData.userAgent} 
+                    // user.loginHistory.push(newObject)
+                    // User.updateOne()
+                    resolve(user)
+                } else {
+                    reject("Incorrect login details")
+                }
+            }).catch((err) => {
+                reject("Decryption error: "+err)
+            })
+
+        }).catch((err) => {
+            reject("Incorrect login details")
+        })
+    })
+}
+
 
 
 module.exports = {
     initialize,
+    register,
+    login
 }
